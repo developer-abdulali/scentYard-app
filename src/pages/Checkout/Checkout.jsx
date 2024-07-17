e2e80d14586ed2b808b31015f71de0d9df59b718
developer-abdulali <ab.ali.soomro@gmail.com>
2024-07-18 02:03:21 +0500

    added changes

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,3 +1,103 @@
+// import { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { Navbar } from "../../components/Navbar/Navbar";
+// import { Footer } from "../../components/Footer/Footer";
+// import { useAddress } from "../../contexts/addressContext";
+// import { useCart } from "../../contexts/cartContext";
+// import { OrderDetails } from "./OrderDetails";
+// import { AddressModal } from "./../../components/AddressModal/AddressModal";
+
+// export const Checkout = () => {
+//   const {
+//     addressState: { addresses, selectedAddrId },
+//     dispatchAddress,
+//   } = useAddress();
+//   const { cartState } = useCart();
+
+//   const [showAddrModal, setShowAddrModal] = useState(false);
+
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     if (cartState.length === 0) {
+//       navigate("/products");
+//     }
+//   }, [cartState]);
+
+//   return (
+//     <div className="min-h-screen flex flex-col">
+//       <Navbar />
+
+//       <section className="flex-grow flex flex-col items-center justify-center bg-gray-100">
+//         <h3 className="text-2xl font-bold text-center">Checkout</h3>
+
+//         <div className="grid grid-cols-2 gap-8 max-w-6xl w-full mx-auto my-4">
+//           <div className="space-y-4">
+//             <div className="text-xl font-bold text-center border-b border-gray-200 pb-2">
+//               Select Address
+//             </div>
+
+//             <div className="space-y-2">
+//               {addresses?.length ? (
+//                 addresses?.map((address) => (
+//                   <label
+//                     className="flex items-start gap-2 p-2 border border-gray-200 rounded"
+//                     key={address._id}
+//                   >
+//                     <input
+//                       type="radio"
+//                       name="address"
+//                       checked={selectedAddrId === address._id}
+//                       onChange={() =>
+//                         dispatchAddress({
+//                           type: "SET_ADDRESS_ID",
+//                           payload: address._id,
+//                         })
+//                       }
+//                       className="text-blue-500"
+//                     />
+
+//                     <div className="text-sm">
+//                       <div className="font-bold">{address.name}</div>
+//                       <div>{address.street},</div>
+//                       <div>
+//                         {address.city} - {address.zipcode}
+//                       </div>
+//                       <div>
+//                         {address.state}, {address.country}
+//                       </div>
+//                       <div>{address.mobile}</div>
+//                     </div>
+//                   </label>
+//                 ))
+//               ) : (
+//                 <p>No address available.</p>
+//               )}
+//             </div>
+
+//             <button
+//               className="border border-blue-500 text-blue-500 px-4 py-2 rounded flex items-center gap-2"
+//               onClick={() => setShowAddrModal(true)}
+//             >
+//               <i className="fas fa-plus"></i>Add address
+//             </button>
+//           </div>
+
+//           <OrderDetails />
+//         </div>
+
+//         {showAddrModal ? (
+//           <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+//             <AddressModal setShowAddrModal={setShowAddrModal} />
+//           </div>
+//         ) : null}
+//       </section>
+
+//       <Footer />
+//     </div>
+//   );
+// };
+
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "../../components/Navbar/Navbar";
@@ -28,10 +128,10 @@ export const Checkout = () => {
     <div className="min-h-screen flex flex-col">
       <Navbar />
 
-      <section className="flex-grow flex flex-col items-center justify-center bg-gray-100">
-        <h3 className="text-2xl font-bold text-center">Checkout</h3>
+      <section className="flex-grow flex flex-col items-center justify-center bg-gray-100 px-4">
+        <h3 className="text-2xl font-bold text-center my-4">Checkout</h3>
 
-        <div className="grid grid-cols-2 gap-8 max-w-6xl w-full mx-auto my-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl w-full mx-auto my-4">
           <div className="space-y-4">
             <div className="text-xl font-bold text-center border-b border-gray-200 pb-2">
               Select Address
@@ -86,11 +186,11 @@ export const Checkout = () => {
           <OrderDetails />
         </div>
 
-        {showAddrModal ? (
+        {showAddrModal && (
           <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
             <AddressModal setShowAddrModal={setShowAddrModal} />
           </div>
-        ) : null}
+        )}
       </section>
 
       <Footer />
--- src/pages/Checkout/OrderDetails.jsx
+++ src/pages/Checkout/OrderDetails.jsx
@@ -1,3 +1,195 @@
+// import toast from "react-hot-toast";
+// import { v4 as uuid } from "uuid";
+// import { useNavigate } from "react-router-dom";
+// import { useCart } from "../../contexts/cartContext";
+// import { useAddress } from "../../contexts/addressContext";
+// import { getTotalPrice } from "../../utils/cartPrice";
+// import { useAuth } from "../../contexts/authContext";
+// import { useOrder } from "../../contexts/orderContext";
+// import { addOrderService } from "../../services/orderServices/addOrderService";
+
+// export const OrderDetails = () => {
+//   const navigate = useNavigate();
+//   const { token } = useAuth();
+
+//   const { cartState, setSelectedCoupon, selectedCoupon, cartPrice } = useCart();
+//   const {
+//     addressState: { addresses, selectedAddrId },
+//   } = useAddress();
+//   const { dispatchOrder } = useOrder();
+
+//   const user = JSON.parse(localStorage.getItem("user"));
+
+//   const totalPrice = getTotalPrice(
+//     cartState,
+//     cartPrice.price,
+//     cartPrice.discountInPrice,
+//     cartPrice.deliveryCharges
+//   );
+
+//   const discountedCouponPrice = (totalPrice * selectedCoupon.discount) / 100;
+
+//   const currentAddress = addresses?.find(
+//     (address) => address._id === selectedAddrId
+//   );
+
+//   const loadScript = async (url) => {
+//     return new Promise((resolve) => {
+//       const script = document.createElement("script");
+//       script.src = url;
+
+//       script.onload = () => {
+//         resolve(true);
+//       };
+
+//       script.onerror = () => {
+//         resolve(false);
+//       };
+//       document.body.appendChild(script);
+//     });
+//   };
+
+//   const displayRazorpay = async () => {
+//     const res = await loadScript(
+//       "https://checkout.razorpay.com/v1/checkout.js"
+//     );
+
+//     if (!res) {
+//       toast.error("Razorpay SDK failed to load, check your connection");
+//       return;
+//     }
+
+//     const options = {
+//       key: "rzp_test_SP206ka3zuV4SX",
+//       amount:
+//         (discountedCouponPrice
+//           ? totalPrice - discountedCouponPrice
+//           : totalPrice) * 100,
+//       currency: "INR",
+//       name: "Essence",
+//       description: "Thank you for shopping with us",
+//       handler: async function (response) {
+//         const orderId = uuid().toString().split("-")[0];
+
+//         const orderData = {
+//           products: [...cartState],
+//           amount: totalPrice,
+//           paymentId: response.razorpay_payment_id,
+//           orderId,
+//           delivery: currentAddress,
+//         };
+
+//         const { data, status } = await addOrderService(orderData, token);
+
+//         if (status === 201) {
+//           setSelectedCoupon({});
+//           dispatchOrder({ type: "GET_ORDERS", payload: data.order });
+//           navigate(`/order/${orderId}`);
+//         }
+//       },
+
+//       prefill: {
+//         name: `${user.firstName} ${user.lastName}`,
+//         email: user.email,
+//         contact: "93225088143",
+//       },
+//     };
+
+//     const paymentObject = new window.Razorpay(options);
+//     paymentObject.open();
+//   };
+
+//   return (
+//     <div className="border border-gray-200 rounded-lg">
+//       <div className="text-xl font-bold text-center border-b border-gray-200 pb-2">
+//         Order Summary
+//       </div>
+
+//       <div className="p-2 space-y-2">
+//         {cartState.map((cartItem) => (
+//           <div key={cartItem.id} className="flex justify-between text-sm">
+//             <div>
+//               {cartItem.title} (&#8377;{cartItem.price} x {cartItem.qty})
+//             </div>
+
+//             <div>&#8377;{cartItem.price * cartItem.qty}</div>
+//           </div>
+//         ))}
+//       </div>
+
+//       <div className="text-xl font-bold text-center border-b border-gray-200 pb-2">
+//         Price Details
+//       </div>
+
+//       <div className="p-2 space-y-2">
+//         <div className="flex justify-between text-sm">
+//           <div>Total Price</div>
+//           <div>&#8377;{cartPrice.price}</div>
+//         </div>
+
+//         <div className="flex justify-between text-sm">
+//           <div>Total Discount</div>
+//           <div>
+//             &#8377;
+//             {cartPrice.discountInPrice +
+//               (JSON.stringify(selectedCoupon) !== "{}"
+//                 ? discountedCouponPrice
+//                 : 0)}
+//           </div>
+//         </div>
+
+//         <div className="flex justify-between text-sm">
+//           <div>Delivery Charges</div>
+//           <div>&#8377;49</div>
+//         </div>
+
+//         <div className="flex justify-between text-sm">
+//           <div>Grand Total</div>
+//           <div>
+//             &#8377;
+//             {totalPrice -
+//               (JSON.stringify(selectedCoupon) !== "{}"
+//                 ? discountedCouponPrice
+//                 : 0)}
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="text-xl font-bold text-center border-b border-gray-200 pb-2">
+//         Deliver To
+//       </div>
+
+//       <div className="p-2 space-y-1 text-sm">
+//         {currentAddress ? (
+//           <>
+//             <div className="font-bold">{currentAddress.name}</div>
+//             <div>
+//               {currentAddress.street}, {currentAddress.city} -{" "}
+//               {currentAddress.zipcode}
+//             </div>
+//             <div>
+//               {currentAddress.state}, {currentAddress.country}
+//             </div>
+//             <div>{currentAddress.mobile}</div>
+//           </>
+//         ) : (
+//           <p>Add an address to proceed.</p>
+//         )}
+//       </div>
+
+//       <button
+//         className={`bg-primary hover:bg-primary/90 text-white px-4 py-2 rounded w-full mt-2 ${
+//           !selectedAddrId ? "opacity-50 cursor-not-allowed" : ""
+//         }`}
+//         disabled={!selectedAddrId}
+//         onClick={displayRazorpay}
+//       >
+//         Place Order
+//       </button>
+//     </div>
+//   );
+// };
+
 import toast from "react-hot-toast";
 import { v4 as uuid } from "uuid";
 import { useNavigate } from "react-router-dom";
@@ -100,14 +292,17 @@ export const OrderDetails = () => {
   };
 
   return (
-    <div className="border border-gray-200 rounded-lg">
+    <div className="border border-gray-200 rounded-lg p-4 md:p-6 lg:p-8">
       <div className="text-xl font-bold text-center border-b border-gray-200 pb-2">
         Order Summary
       </div>
 
-      <div className="p-2 space-y-2">
+      <div className="space-y-2 md:space-y-4 lg:space-y-6">
         {cartState.map((cartItem) => (
-          <div key={cartItem.id} className="flex justify-between text-sm">
+          <div
+            key={cartItem.id}
+            className="flex justify-between text-sm md:text-base lg:text-lg"
+          >
             <div>
               {cartItem.title} (&#8377;{cartItem.price} x {cartItem.qty})
             </div>
@@ -117,17 +312,17 @@ export const OrderDetails = () => {
         ))}
       </div>
 
-      <div className="text-xl font-bold text-center border-b border-gray-200 pb-2">
+      <div className="text-xl font-bold text-center border-b border-gray-200 pb-2 mt-4 md:mt-6 lg:mt-8">
         Price Details
       </div>
 
-      <div className="p-2 space-y-2">
-        <div className="flex justify-between text-sm">
+      <div className="space-y-2 md:space-y-4 lg:space-y-6 mt-2 md:mt-4 lg:mt-6">
+        <div className="flex justify-between text-sm md:text-base lg:text-lg">
           <div>Total Price</div>
           <div>&#8377;{cartPrice.price}</div>
         </div>
 
-        <div className="flex justify-between text-sm">
+        <div className="flex justify-between text-sm md:text-base lg:text-lg">
           <div>Total Discount</div>
           <div>
             &#8377;
@@ -138,12 +333,12 @@ export const OrderDetails = () => {
           </div>
         </div>
 
-        <div className="flex justify-between text-sm">
+        <div className="flex justify-between text-sm md:text-base lg:text-lg">
           <div>Delivery Charges</div>
           <div>&#8377;49</div>
         </div>
 
-        <div className="flex justify-between text-sm">
+        <div className="flex justify-between text-sm md:text-base lg:text-lg">
           <div>Grand Total</div>
           <div>
             &#8377;
@@ -155,30 +350,36 @@ export const OrderDetails = () => {
         </div>
       </div>
 
-      <div className="text-xl font-bold text-center border-b border-gray-200 pb-2">
+      <div className="text-xl font-bold text-center border-b border-gray-200 pb-2 mt-4 md:mt-6 lg:mt-8">
         Deliver To
       </div>
 
-      <div className="p-2 space-y-1 text-sm">
+      <div className="space-y-1 md:space-y-2 lg:space-y-3 mt-2 md:mt-4 lg:mt-6">
         {currentAddress ? (
           <>
-            <div className="font-bold">{currentAddress.name}</div>
-            <div>
+            <div className="font-bold text-sm md:text-base lg:text-lg">
+              {currentAddress.name}
+            </div>
+            <div className="text-sm md:text-base lg:text-lg">
               {currentAddress.street}, {currentAddress.city} -{" "}
               {currentAddress.zipcode}
             </div>
-            <div>
+            <div className="text-sm md:text-base lg:text-lg">
               {currentAddress.state}, {currentAddress.country}
             </div>
-            <div>{currentAddress.mobile}</div>
+            <div className="text-sm md:text-base lg:text-lg">
+              {currentAddress.mobile}
+            </div>
           </>
         ) : (
-          <p>Add an address to proceed.</p>
+          <p className="text-sm md:text-base lg:text-lg">
+            Add an address to proceed.
+          </p>
         )}
       </div>
 
       <button
-        className={`bg-primary hover:bg-primary/90 text-white px-4 py-2 rounded w-full mt-2 ${
+        className={`bg-primary hover:bg-primary/90 text-white px-4 py-2 rounded w-full mt-4 md:mt-6 lg:mt-8 ${
           !selectedAddrId ? "opacity-50 cursor-not-allowed" : ""
         }`}
         disabled={!selectedAddrId}
